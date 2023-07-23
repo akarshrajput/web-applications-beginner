ba43545e5effd31e4853de609d81fc458c3bb54a
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2023-07-23 19:37:20 +0530

    Delete a Private Application code

--- 5-your-web-docs-main/script.js
+++
@@ -1,5 +0,0 @@
-const btnEl = document.querySelector(".btn-nav");
-const btnToogle = document.querySelector(".navigation");
-btnEl.addEventListener("click", function () {
-  btnToogle.classList.toggle("nav-open");
-});
